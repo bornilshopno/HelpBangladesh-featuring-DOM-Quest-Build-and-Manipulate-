0680cbbae90844c009bb7fa896e98aea7639492a
Ashraf Manna <optimisticashraf@gmail.com>
2024-09-24 23:47:44 +0600

    final commit

--- JS/blog.js
+++ JS/blog.js
@@ -2,6 +2,3 @@ document.getElementById('btn-home').addEventListener('click', function(){
     window.location.href="./index.html"
 })
 
-// // const mainBalance = getValueByTextIDdoc('main-balance')
-// const mainBalance2=document.getElementById('main-balance').innerText
-// document.getElementById('amountInBlog').innerText =mainBalance2; //Update main balance in blog page
